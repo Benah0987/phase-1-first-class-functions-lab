20a5233032c17638dd108d9324f695689733f944
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-07 07:55:53 +0300

    defined an anonymous function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+//array of drivers
+const drivers = ["Bena","Kim", "Nduch", "Abiyah"]
+//defined an anonymous funtion
+const returnFirstTwoDrivers = function (drivers){
+     return drivers.slice(0,2);
+     //used the slice method to cut the drivers 
+}
+console.log(returnFirstTwoDrivers);
+
+///function two
+//defined an anonymous function
+
