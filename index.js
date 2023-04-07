bc81338b9d532e400e2bf52629c62daa846bb14a
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-07 08:16:45 +0300

    the function takes in an argument createFareMultiplier

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //array of drivers
 const drivers = ["Bena","Kim", "Nduch", "Abiyah"]
 //defined an anonymous funtion
+//accept an array of driver as an argument
 const returnFirstTwoDrivers = function (drivers){
      return drivers.slice(0,2);
      //used the slice method to cut the drivers 
@@ -10,4 +11,23 @@ console.log(returnFirstTwoDrivers);
 
 ///function two
 //defined an anonymous function
+const returnLastTwoDrivers = function (){
+    return drivers.slice(-2)
+    //added a slice method to obtain the last 2 elements
+    //used a negative index so the array starts from the end
+    //and count back 2 elements
+}
+console.log(returnLastTwoDrivers())
 
+//array containing two elements
+//declares a constant variable named selectingDrivers
+//array is an object n the functions can be passed as variables
+const selectingDrivers = [returnLastTwoDrivers, returnFirstTwoDrivers]
+
+//declares a const variable named and i assigend an anonymuos function
+//the function takes in an argument
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    }
+  }
